aa62817d0340e1c8c81c6f32acdf42eca37a0ffc
Andrei Oprisan <andrei@oprisan.com>
2013-10-16 19:24:42 -0400

    add node-expat dependency

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   summary: "Meteor bindings for Spreedly payment tokenization and charge processing"
 });
 
-Npm.depends({ "spreedly-node": "0.1.0" });
+Npm.depends({ "spreedly-node": "0.1.0" }, { "node-expat": "2.0.0"});
 
 Package.on_use(function(api) {
   if (api.export) api.export('Spreedly', 'server');        
